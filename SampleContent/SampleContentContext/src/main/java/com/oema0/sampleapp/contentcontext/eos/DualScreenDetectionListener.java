9b86c45bd99ddb072aba52580080336bbe44f9ec
ken.cheng <v-yuach@microsoft.com>
2019-11-12 10:37:49 +0800

    Upload conten context

---
+++ SampleContent/SampleContentContext/src/main/java/com/oema0/sampleapp/contentcontext/eos/DualScreenDetectionListener.java
@@ -0,0 +1,9 @@
+package com.oema0.sampleapp.contentcontext.eos;
+
+import android.graphics.Rect;
+
+// Interface to notify listeners of changes to app layout in two screen layouts
+public interface DualScreenDetectionListener {
+    void useSingleScreenMode(int rotation);
+    void useDualScreenMode(int rotation, Rect screenRect1, Rect screenRect2);
+}
