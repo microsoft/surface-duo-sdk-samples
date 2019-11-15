0b9c68494a834983f2d1f6efad24a58d03c5fd38
ken.cheng <v-yuach@microsoft.com>
2019-11-15 11:02:53 +0800

    renamed package name

---
+++ contentcontext/src/main/java/com/microsoft/device/display/samples/contentcontext/ItemsListFragment.java
@@ -0,0 +1,92 @@
+package com.microsoft.device.display.samples.contentcontext;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+
+public class ItemsListFragment extends Fragment implements ListView.OnItemClickListener {
+	private ArrayAdapter<Item> adapterItems;
+	private ListView lvItems;
+	private static final String TAG = "ItemsListFragment";
+
+	private OnItemSelectedListener listener;
+
+	public interface OnItemSelectedListener {
+		void onItemSelected(Item i, int position);
+		void onInit(Item i, int position);
+	}
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		if (activity instanceof OnItemSelectedListener) {
+			listener = (OnItemSelectedListener) activity;
+		} else {
+			throw new ClassCastException(activity.toString()
+					+ " must implement ItemsListFragment.OnItemSelectedListener");
+		}
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		ArrayList<Item> items = Item.getItems();
+		adapterItems = new ArrayAdapter<Item>(getActivity(),
+				android.R.layout.simple_list_item_activated_1, items);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_items_list, container, false);
+		lvItems = (ListView) view.findViewById(R.id.lvItems);
+		lvItems.setAdapter(adapterItems);
+		lvItems.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+		lvItems.setOnItemClickListener(this);
+		Bundle arguements = getArguments();
+		if(arguements != null) {
+			int position = arguements.getInt(MainActivity.POSITION_KEY);
+			Log.d(TAG,"position " + position);
+			lvItems.setItemChecked(position, true);
+			listener.onInit(adapterItems.getItem(position), position);
+			setArguments(null);
+		}
+		
+		return view;
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> adapterView, View item, int position, long rowId) {
+		Item i = adapterItems.getItem(position);
+		listener.onItemSelected(i, position);
+	}
+
+	public static ItemsListFragment newInstance() {
+    	ItemsListFragment fragment = new ItemsListFragment();
+        return fragment;
+	}
+	
+	public static ItemsListFragment newInstance(int position) {
+		ItemsListFragment fragment = new ItemsListFragment();
+		Bundle bundle = new Bundle();
+		bundle.putInt(MainActivity.POSITION_KEY, position);
+		fragment.setArguments(bundle);
+        return fragment;
+    }
+
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+		Log.d(TAG,"onDestroyView");
+	}
+}
---
+++ twopage/src/main/java/com/microsoft/device/display/samples/twopage/TestFragment.java
@@ -0,0 +1,98 @@
+package com.microsoft.device.display.samples.twopage;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class TestFragment extends Fragment {
+
+    private static final String TAG = TestFragment.class.getSimpleName();
+    private TextView mTextView;
+    private String text;
+
+    public static TestFragment newInstance(String text){
+        TestFragment testFragment = new TestFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("text", text);
+        testFragment.setArguments(bundle);
+        return testFragment;
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        text = getArguments().getString("text");
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: "+ text);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_layout, container, false);
+        mTextView = (TextView) view.findViewById(R.id.textview);
+        mTextView.setText(text);
+        Log.d(TAG, "onCreateView: ");
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d(TAG, "onDestroyView: ");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+    @Override
+    public String toString() {
+        return text;
+    }
+
+    public static SparseArray<TestFragment> getFragments() {
+        SparseArray<TestFragment> fragments = new SparseArray<>();
+        for(int i = 0; i < 10; i++) {
+            fragments.put(i, TestFragment.newInstance(Integer.toString(i)));
+        }
+        return fragments;
+    }
+}
