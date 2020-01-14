74dfabd3d9d44564cfd12e7b4d6088e1c1090fce
Roxana Botosaneanu <v-roboto@microsoft.com>
2020-01-14 13:55:49 +0200

    Control flow issues

--- DualView/src/main/java/com/microsoft/device/display/samples/contentcontext/MainActivity.java
+++ DualView/src/main/java/com/microsoft/device/display/samples/contentcontext/MainActivity.java
@@ -131,10 +131,9 @@ public class MainActivity extends AppCompatActivity implements BaseFragment.OnIt
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                return true;
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
